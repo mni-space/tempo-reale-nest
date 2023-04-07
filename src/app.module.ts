159a96384cb0a75b5376d196294882e14f11a74e
_lmmmmmm <lmmmmmm12138@gmail.com>
2023-04-08 00:10:13 +0800

    fix: deploy to zeabur

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
 import { EventsModule } from './events/events.module';
 
 @Module({
-  imports: [EventsModule],
+  imports: [ConfigModule.forRoot(), EventsModule],
   controllers: [AppController],
   providers: [AppService],
 })
