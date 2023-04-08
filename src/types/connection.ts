289e45afd34d15712662ff215a8bc6d0750b3a3f
_lmmmmmm <lmmmmmm12138@gmail.com>
2023-04-08 11:56:08 +0800

    chore: remove message type

--- src/types/connection.ts
+++ src/types/connection.ts
@@ -1,21 +1,15 @@
-import { MessageTypeEnum } from './enum';
-
 export interface MessageConnection {
-  id: string;
   to: string;
 }
 
 export interface OfferMessage extends MessageConnection {
-  type: MessageTypeEnum.Offer;
   offer: RTCSessionDescriptionInit;
 }
 
 export interface AnswerMessage extends MessageConnection {
-  type: MessageTypeEnum.Answer;
   answer: RTCSessionDescriptionInit;
 }
 
 export interface CandidateMessage extends MessageConnection {
-  type: MessageTypeEnum.Candidate;
   candidate: RTCIceCandidateInit;
 }
